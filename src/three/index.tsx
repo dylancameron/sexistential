83aa77cd236d3b7fb75d0f06871ccde2eed87744
Dylan <dylancameron@beggars.com>
2025-12-16 10:28:46 -0500

    Removed old sparkler code. Added vercel.json file for root rewrites

---
+++ src/three/index.tsx
@@ -0,0 +1,23 @@
+import {
+	AdditiveBlending,
+	BufferAttribute,
+	BufferGeometry,
+	Color,
+	Points,
+	ShaderMaterial,
+	Vector3,
+	DynamicDrawUsage,
+	PointLight,
+} from "three";
+
+export {
+	AdditiveBlending,
+	BufferAttribute,
+	BufferGeometry,
+	Color,
+	Points,
+	ShaderMaterial,
+	Vector3,
+	DynamicDrawUsage,
+	PointLight,
+};
